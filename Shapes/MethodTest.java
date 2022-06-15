f0a16884dfc91133ad2ba503d192c5a126ca6b4e
jopulation <jopulation@yonsei.ac.kr>
2022-06-16 01:46:49 +0900

    Abstract class & abstract method - 1

---
+++ Shapes/MethodTest.java
@@ -0,0 +1,9 @@
+public class MethodTest {
+    public static void main(String[] args) {
+        GraphicsObject r = new Rectangle();
+
+        r.draw();
+    }
+}
+
+
---
+++ Shapes/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle extends GraphicsObject {
+    private int width;
+    private int height;
+
+    public Rectangle() {
+        width = 0;
+        height = 0;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(width + " x " + height);
+    }
+}
+
+
+
