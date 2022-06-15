7db45ddcdc517678ac737a1dbcf814ebee9d18a6
jopulation <jopulation@yonsei.ac.kr>
2022-06-15 18:37:11 +0900

    Wls Game

--- Game/GameCharacter.java
+++ Game/GameCharacter.java
@@ -4,6 +4,8 @@ public class GameCharacter {
     private int healthPoint;
     private int strength;
 
+    private GamePlay game;
+
     /* Constructors */
     public GameCharacter(String characterName) {
         name = characterName;
@@ -14,22 +16,17 @@ public class GameCharacter {
     /* Method */
 
     public void printStatus() {
-        System.out.println("# Character information: ");
         System.out.println("Name: "+ name);
         System.out.println("Health point: " + healthPoint);
         System.out.println("Strength: " + strength);
-        System.out.println();
     }
-    private int randomGenerator(int size) {
-        return (int)(Math.random() * (size+1));
-    }
-    private int generateAttackHitPoint() {
-        return randomGenerator(strength);
-    }
-    public int attack(GameCharacter enemy) {
-        int hitPoint = generateAttackHitPoint();
-        enemy.setHealthPoint(enemy.getHealthPoint() - hitPoint);
-        return hitPoint;
+
+    public int attack(GameCharacter opponent) {
+        int damage = game.generateAttackHitPoint(strength);
+
+        opponent.setHealthPoint(opponent.getHealthPoint() - damage);
+
+        return damage;
     }
     public boolean isDead() {
         return (healthPoint <= 0);
@@ -40,20 +37,31 @@ public class GameCharacter {
         return name;
     }
 
-    public int getHealthPoint() {
-        return healthPoint;
+    public void setName(String updateName) {
+        name = updateName;
     }
 
-    public int getStrength() {
-        return strength;
+    public int getHealthPoint() {
+        return healthPoint;
     }
 
     public void setHealthPoint(int healthPoint) {
         this.healthPoint = healthPoint;
     }
 
+    public int getStrength() {
+        return strength;
+    }
+
     public void setStrength(int strength) {
         this.strength = strength;
     }
 
+    public GamePlay getGame() {
+        return game;
+    }
+
+    public void setGame(GamePlay g) {
+        game = g;
+    }
 }
--- Game/GamePlay.java
+++ Game/GamePlay.java
@@ -1,38 +1,65 @@
 public class GamePlay {
-    public static void main(String[] args) {
-        GameCharacter player, monster;
+    /* instance variable */
+    GameCharacter player, monster;
+    private String gameName;
 
+    // Radom number generator
+
+    public GamePlay() {
+        gameName = "";
+    }
+
+    public GamePlay(String name) {
+        gameName = name;
+    }
+
+    public int generateAttackHitPoint(int strength) {
+        int hitPoint = 0;
+        hitPoint = (int)(Math.random() * strength) +1;  // from 1 to strength
+        return hitPoint;
+    }
+    
+    public void play() {
         player = new GameCharacter("Foobar the Warrior");
         monster = new GameCharacter("Slime the Great");
+    
+        // pass instance information of GamePlay to GameCharacter instances
 
-        int round = 1;
-
+        player.setGame(this);
+        monster.setGame(this);
+    
         System.out.println("###############");
         System.out.println("### Game starts");
         System.out.println("###############");
         System.out.println();
+    
+        int round = 1;
+        int damage = 0;
 
         while(true) {
             System.out.println("### round "+ round);
-
+    
             player.printStatus();
             monster.printStatus();
-
+            System.out.println();
+    
             /* player attacks monster */
-            int damage = player.attack(monster);
+
+            damage = player.attack(monster);
             System.out.println(player.getName() + " attacks " + monster.getName());
             System.out.println(monster.getName() + " gets damage of " + damage);
-
+    
             if(monster.isDead() == true) {
                 System.out.println(player.getName() + " killed " + monster.getName() + "!");
                 break;
             }
-
+    
             /* monster attacks player */
+
             damage = monster.attack(player);
             System.out.println(monster.getName() + " attacks " + player.getName());
             System.out.println(player.getName() + " gets damage of " + damage);
-
+    
             if(player.isDead() == true) {
                 System.out.println(monster.getName() + " killed " + player.getName() +"!");
                 break;
@@ -40,8 +67,7 @@ public class GamePlay {
             round = round + 1;
             System.out.println();
         }
+        System.out.println();
         System.out.println("### Game over at round " + round);
     }
-}
-
 }
